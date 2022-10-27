8bd3252516d05fa7cfdda050c261509cb94747f5
Rodrigo Brugiolo <99890204+rodrigobrugiolo@users.noreply.github.com>
2022-10-27 01:19:51 -0300

    Add files via upload

---
+++ alura/aula1.js
@@ -0,0 +1,18 @@
+var nome = "Rodrigo"
+var valorEmReal = 64.55
+var taxaCambio = 5.18
+//valor de cambio para o dia 14/09 as 16:30
+
+var valorFinal = (valorEmReal * taxaCambio) 
+
+
+
+
+console.log("Bem vindo " + nome + ", o valor do cambio do real para o dolar no dia de hoje, é de " + taxaCambio.toFixed(2) + " o item no valor de " + valorEmReal + "R$ foi convertido através da taxa dada anteriormente e foi para o valor de " + valorFinal.toFixed(2) +
+"U$, porém, pelas leis do estado, temos que acrescentar o valor de 6% de imposto em cima do produto, que assim, irá para o valor de " + (valorFinal*1.06).toFixed(2) + "U$. Agradecemos pela compra, e tenha uma ótima semana")
+
+
+
+
+// tentativa de utilização do JS
+
